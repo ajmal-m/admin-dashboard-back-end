545ac67a42ad1aa88c3b58f3b141959099682d65
ajmal-m <ajua76774@gmail.com>
2025-12-06 15:06:18 +0530

    Product get updates

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,22 @@ const Product = require("../models/product");
 const cloudinary = require("../config/cloudinary");
 
 
+module.exports.getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json({
+            message:"Product retrieved successfully.",
+            data: products
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:"Internal server error"
+        });
+    }
+}
+
+
 module.exports.addProducts = async (req, res) => {
     try {
         const image = req.file;
--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addProducts } = require('../controllers/product-controller');
+const { addProducts , getProducts } = require('../controllers/product-controller');
 
 const multer = require("multer");
 const upload = multer({ dest: 'uploads/' })
 
 router.post('/add', upload.single("image") , addProducts);
+router.get("/all", getProducts);
 
 module.exports = router;
