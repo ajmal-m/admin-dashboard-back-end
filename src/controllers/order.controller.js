f22e21563c57fa6be38a5d8541a0aaa9773365b7
ajmal-m <ajmalm76774@gmail.com>
2025-12-18 23:27:00 +0530

    Order api updates

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,12 +1,44 @@
 const Order = require("../models/order");
 
 
+module.exports.getOrdersByUserId = async (req, res) => {
+    try {
+        
+        const orders = await Order.find({ userId : req.params.id });
+        res.status(200).json({
+            data:orders,
+            message:"Order fetched sucessfully"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:"Internal server error"
+        });
+    }
+};
+
+
 module.exports.getOrderByUser = async (req, res) => {
     try {
         
         const orders = await Order.find();
         res.status(200).json({
             data:orders,
+            message:"Order fetched sucessfully"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:"Internal server error"
+        });
+    }
+}
+
+module.exports.getOrderByOrderId = async (req, res)=> {
+     try {
+        const order = await Order.findById(req.params.id).populate("items.productId");
+        res.status(200).json({
+            data:order,
             message:"Order fetched sucessfully"
         })
     } catch (error) {
--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, getOrderByUser } = require('../controllers/order.controller');
+const { createOrder, getOrderByUser, getOrdersByUserId , getOrderByOrderId } = require('../controllers/order.controller');
 
 
+router.get("/:id", getOrderByOrderId);
+router.get("/user/:id", getOrdersByUserId);
 router.get("/", getOrderByUser ) ;
 router.post("/create", createOrder);
 
