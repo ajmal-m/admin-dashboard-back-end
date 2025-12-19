31df87ef45347d1da6b3198131c2702afa9b08f2
ajmal-m <ajmalm76774@gmail.com>
2025-12-19 20:29:25 +0530

    Update order status updates

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -58,6 +58,38 @@ module.exports.getOrderByOrderId = async (req, res)=> {
 }
 
 
+module.exports.updateOrderStatus = async (req, res) =>{
+    try {
+        const updateQuery = {};
+        const data = req.body;
+        if(!data?.id){
+            return res.status(404).json({ message:"Order Id is missing" })
+        }
+        if(data?.orderStatus){
+            updateQuery["orderStatus"] = data.orderStatus;
+        }
+        if(data?.paymentStatus){
+            updateQuery["payment.status"] = data.paymentStatus;
+        }
+        const updatedval =  await Order.findByIdAndUpdate(
+            data.id,
+            {
+                $set: updateQuery
+            }
+        );
+        res.status(200).json({
+            data: updatedval,
+            message:"Order status updated successfully."
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:"Internal server error"
+        });
+    }
+}
+
+
 module.exports.createOrder = async(req, res) => {
     try {
         if(!req.body.order){
--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, getAllOrders, getOrdersByUserId , getOrderByOrderId } = require('../controllers/order.controller');
+const { createOrder, getAllOrders, getOrdersByUserId , getOrderByOrderId,
+    updateOrderStatus
+ } = require('../controllers/order.controller');
 
 
 router.get("/:id", getOrderByOrderId);
 router.get("/user/:id", getOrdersByUserId);
 router.get("/", getAllOrders ) ;
 router.post("/create", createOrder);
+router.put("/update-order", updateOrderStatus);
 
 module.exports = router; 
